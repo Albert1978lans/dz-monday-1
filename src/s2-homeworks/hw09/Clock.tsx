d0223035f3651edf9c58f3318eb3b2b73d3b6791
Albert <bert21387@gmail.com>
2023-04-01 01:21:03 +0500

    dz sreda-1-2

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -47,11 +47,11 @@ function Clock() {
 
     const stringTime = `${getValue('ru', {hour: "numeric", minute: "numeric", second: "numeric"})}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
 
-    const stringDate = `${getValue('en', {month: "long"})}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${getValue('ru', {day: 'numeric', month: 'numeric', year: 'numeric'})}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = `${getValue('en', {weekday: "long"})}` || <br/> // пишут студенты
-    const stringMonth = `${getValue('ru', {day: 'numeric', month: 'numeric', year: 'numeric'})}` || <br/> // пишут студенты
+    const stringMonth = `${getValue('en', {month: "long"})}` || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
